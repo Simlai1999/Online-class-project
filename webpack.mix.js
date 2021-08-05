d8f9c44188e68c8345887a944b63a11cdd521ab0
Sounak Simlai <sounaksimlai@gmail.com>
2021-08-05 08:53:51 +0530

    live server working

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,4 @@ const { mix } = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
    .copy('resources/index.html', 'public')
-   .browserSync();
+   .browserSync('http://127.0.0.1:5500');
