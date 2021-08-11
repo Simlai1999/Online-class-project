9388ddca115fbb9c4c3b9d29e43c68439a04ca21
Sounak Simlai <sounaksimlai@gmail.com>
2021-08-11 19:46:07 +0530

    redom working

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
-// import { el, mount } from "https://redom.js.org/redom.es.min.js";
+// import redom from "https://redom.js.org/redom.es.min.js";
+import * as redom from "../../node_modules/redom/dist/redom.min.js";
+console.log(redom);
+const {el, mount} = redom;
+console.log(mount);
+    class Hello {
+      constructor() {
+        this.el = el("h1");
+      }
+      update(data) {
+        this.el.textContent = "Hello " + data + "!";
+      }
+    }
+
+    const hello = new Hello();
+
+    hello.update("RE:DOM!");
+
+    mount(document.body, hello);
--- resources/js/sidebar.js
+++ resources/js/sidebar.js
@@ -1,7 +1,18 @@
-function openNav() {
-    document.getElementById("sidebar").style.width = "250px";
-}
 
-export function closeNav() {
-    document.getElementById("sidebar").style.width = "0";
-}
+{/* <div class="menu-container" id="sidebar">
+        <span class="fas fa-times" onclick="closeNav()"></span>
+        <div class="menu-wrapper">
+            <aside class="menu">
+                <ul class="menu-list">
+                    <li>
+                        <a class="is-active"><span class="fas fa-clipboard-list"></span></i>Home</a>
+                    </li>
+                    <li><a><span class="fas fa-chalkboard-teacher"></span>Lectures</a></li>
+                    <li><a><span class="fas fa-calendar"></span>Schedules</a></li>
+                    <li><a><span class="fas fa-pen-square"></span>Notes</a></li>
+                    <li class="end"><a><span class="fas fa-cog"></span>Settings</a></li>
+                </ul>
+            </aside>
+        </div>
+    </div>
+         */}
