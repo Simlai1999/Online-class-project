0db1c9edc2ab7645843c3ad55504e9d4098c84bb
Sounak Simlai <sounaksimlai@gmail.com>
2021-09-26 19:40:44 +0530

    minor changes

--- resources/js/pages/schedules-page.js
+++ resources/js/pages/schedules-page.js
@@ -1,47 +1,30 @@
 import { el } from "redom";
+import { getWeekOfMonth, eachDayOfInterval, startOfWeek, endOfWeek } from "date-fns";
 
 export class SchedulesPage {
     constructor() {
-        const weeks = [
-            [
-                1, 2, 3, 4, 5, 6, 7
-            ],
-            [
-                8, 9, 10, 11, 12, 13, 14
-            ],
-            [
-                15, 16, 17, 18, 19, 20, 21
-            ],
-            [
-                22, 23, 24, 25, 26, 27, 28, 29, 30
-            ],
-        ];
 
         const date = new Date();
+        const daysInCurrentWeek = eachDayOfInterval({ start: startOfWeek(date), end: endOfWeek(date) });
+        // console.log(daysInCurrentWeek);
 
-        const today = date.getDate();
-
-        weeks.forEach(week => {
-            week.forEach(day => {
-                if (day == today) {
-                    this.date = week;
-                }
-            })
-        });
+        for(const day of daysInCurrentWeek){
+            
+        }
 
         this.dayNames = el('div.names',
             el('div.weekdays', 'Sunday'),
             el('div.weekdays', 'Monday'),
-            el('div.weekdays','Tuesday'),
-            el('div.weekdays','Wednesday'),
-            el('div.weekdays','Thursday'),
-            el('div.weekdays','Friday'),
-            el('div.weekdays','Saturday')
+            el('div.weekdays', 'Tuesday'),
+            el('div.weekdays', 'Wednesday'),
+            el('div.weekdays', 'Thursday'),
+            el('div.weekdays', 'Friday'),
+            el('div.weekdays', 'Saturday')
         );
-        this.dates = el('div.dates', this.date);
+        // this.dates = el('div.dates', this.date);
 
         this.calender = el('div.calender',
-            this.dayNames, this.dates, 
+            this.dayNames, this.dates,
             el('hr.divider')
         );
 
